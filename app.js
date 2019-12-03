23d817feb2f58d256595d658f362828f91312ea8
Noah Gibson <noah.gibson99@gmail.com>
2019-12-03 16:25:52 -0700

    Fix testing suite and pass first test
    Co-authored-by: Zoe Harvey <zoekharvey@gmail.com>

--- app.js
+++ app.js
@@ -10,3 +10,14 @@ app.use(cors());
 // Can pass options 
 app.use(express.json());
 
+app.get('/api/v1/projects', (request, response) => {
+  database('projects').select()
+    .then(projects => {
+      response.status(200).json(projects)
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+})
+
+module.exports = app;
