7697a0df091624411f5f65e912051ff9ddb9b8ee
Noah Gibson <noah.gibson99@gmail.com>
2019-12-04 14:36:02 -0700

    Add tests and endpoints for post
    Co-authored-by: Zoe Harvey <zoekharvey@gmail.com>

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const environment = process.env.NODE_ENV || 'development'
-const configuration = require('./knexfile')[environment]
-const database = require('knex')(configuration)
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
+app.locals.title = 'colorpicker';
 app.use(cors());
 // Can pass options 
 app.use(express.json());
@@ -62,6 +63,46 @@ app.get('/api/v1/palettes/:id', (request, response) => {
     .catch(error => {
       response.status(500).json({ error })
     })
-})
+});
+
+app.post('/api/v1/projects', (request, response) => {
+  const project = request.body;
+
+  for(let requiredParam of ["name"]) {
+    if(!project[requiredParam]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { name: <String> }. You\'re missing a \'${requiredParam}\' property.`})
+    }
+  }
+
+  database('projects').insert(project, 'id')
+    .then(project => {
+      response.status(201).json({ id: project[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+});
+
+app.post('/api/v1/palettes', (request, response) => {
+  const palette = request.body;
+
+  for(let requiredParam of ['name', 'project_id', 'color1', 'color2', 'color3', 'color4', 'color5']) {
+    if(!palette[requiredParam]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { name: <String>, project_id <Integer>, color1: <String>, color2: <String>, color3: <String>, color4: <String>, color5: <String>}. You\'re missing a \'${requiredParam}\' property.`})
+    }
+  }
+
+  database('palettes').insert(palette, 'id')
+    .then(palette => {
+      response.status(201).json({ id: palette[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+});
 
 module.exports = app;
