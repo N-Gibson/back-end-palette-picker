1ebb428bb41257233442c82652085408d723ba3b
Noah Gibson <noah.gibson99@gmail.com>
2019-12-11 11:42:12 -0700

    Rewrite delete endpoint

--- app.js
+++ app.js
@@ -148,26 +148,27 @@ app.patch('/api/v1/palettes/:id', (request, response) => {
   }
 });
 
+
 app.delete('/api/v1/projects/:id', (request, response) => {
   const { id } = request.params
   database('palettes')
-    .where('project_id', id)
-    .del()
-    .then(res => {
-      database('projects')
+  .where('project_id', id)
+  .del()
+  .then(() => {
+    database('projects')
       .where('id', id)
       .del()
-      .then(res => {
-        if(!res) {
+      .then(response => {
+        if(!response) {
           response.status(404).json(`No project with id: ${id} found`)
         } else {
           response.status(204).json(`Project with id: ${id} has been deleted.`)
         }
       })
-      .catch(error => {
-        response.status(500).json({ error })
-      })
-    })
+  })
+  .catch(error => {
+    response.status(500).json({ error })
+  })
 });
 
 // app.delete('/api/v1/projects:id', (request, response) => {
